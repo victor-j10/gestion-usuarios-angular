903ff81b69678edc8f14835f73e5fef8c8f62dcc
victor-j10 <victorjhr2211@gmail.com>
2025-06-06 10:49:54 -0500

    Ajustes finales

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,13 +16,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  //se declaran vacías las variables que recibirán la info de los input
   email = '';
   password = '';
+  //se declara una variable vacía para mandar mensajes de error
   errorMessage = '';
+  //se declara una variable booleana false para controlar el snipper de la carga de la solicitud
   loading = false;
 
+  //se inyecta http para las peticiones y router para la navegación
   constructor(private http: HttpClient, private router: Router) { }
 
+  /*
+  El método login obtiene los datos del db.json y busca una coincidencia con los ingresados
+  en el input. si es correcto guarda los datos del usuario en el localStorgae y lo envía
+  a la vista principal, si algo falla se mostrará un mensaje de error.
+  */
   login() {
     this.loading = true;
 
